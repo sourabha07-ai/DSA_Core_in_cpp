ca75c8d5bc86df33c2e661fb2448d62730fe31ca
Sourabha Jena <sourabhajena2003@gmail.com>
2026-09-03 03:35:29 +0530

    10_empty_method.cpp

---
+++ String_in_C++/ChatGPT_Learning/10_empty_method.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <string>
+using namespace std; 
+
+int main(){
+     string name = "Sourabha!";
+
+    if(name.empty())cout <<boolalpha <<true <<endl;
+    else cout <<boolalpha <<false <<endl;
+        
+  
+    return 0;
+}
