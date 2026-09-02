5a393853cc25d910d904117c08809b2d4981336e
Sourabha Jena <sourabhajena2003@gmail.com>
2026-09-03 01:07:54 +0530

    4_Access_character.cpp

---
+++ String_in_C++/ChatGPT_Learning/4_Access_character.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <vector>
+#include <string>
+using namespace std;
+
+int main()
+{
+        string Student = "Sourabha!";
+        cout << "My name: "
+             << Student[0] << " "
+             << Student[1] << " "
+             << Student[2] << " "
+             << Student[3] << " "
+             << Student[4] << " "
+             << Student[5] << " "
+             << Student[6] << " "
+             << Student[7] << " "
+             << Student[8] << " " << endl;
+
+        return 0;
+}
