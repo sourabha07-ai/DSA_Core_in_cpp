21595165083f9e3436cc12ec78ea8535dfbb03e0
Sourabha Jena <sourabhajena2003@gmail.com>
2026-09-03 01:46:05 +0530

    add 2 files

---
+++ String_in_C++/PW_Learning/1_character_array.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+        char arr[] = {'S','o','u','r','a','b','h','a'};
+        int arr2[] = {1,2,3,4,5,6,7};
+        cout <<arr <<endl;
+        cout <<arr2 <<endl;
+
+}
---
+++ String_in_C++/PW_Learning/2_basic.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+#include<string>
+using namespace std;
+
+int main(){
+//     string x = "Sourabha! is Software engineer.";
+//     cout <<"my name: " << x <<endl;
+
+//        string str;
+//        cout <<"Enter name: ";
+//        cin >> str;
+//        cout <<"Name: " <<str <<endl;
+
+
+       string str;
+       cout <<"Enter name: ";
+       getline(cin,str);
+       cout <<"Name: " <<str <<endl;
+}
