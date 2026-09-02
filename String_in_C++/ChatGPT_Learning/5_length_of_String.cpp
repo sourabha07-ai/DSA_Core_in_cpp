6b5e15107af98ee8b32575c4bfea12856f0f4bfc
Sourabha Jena <sourabhajena2003@gmail.com>
2026-09-03 01:13:42 +0530

    5_length_of_String.cpp

---
+++ String_in_C++/ChatGPT_Learning/5_length_of_String.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <vector>
+#include <string>
+using namespace std; 
+
+int main(){
+        string name = "Sourabha";
+        cout <<"Your name: " <<name <<"." <<"Length is: "<<name.length()<<endl;
+        cout <<"Your name: " <<name <<"." <<"Size is: "<<name.size()<<endl;
+  
+    return 0;
+}
