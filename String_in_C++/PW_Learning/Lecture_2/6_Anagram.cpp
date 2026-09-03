af6a234790507759df6de9e023cbf7d6ddcf1b5d
Sourabha Jena <sourabhajena2003@gmail.com>
2026-09-04 04:20:32 +0530

    add 2 files

---
+++ String_in_C++/PW_Learning/Lecture_2/6_Anagram.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+   string str;
+   cout <<"Enter String 1: ";
+   getline(cin,str);
+   
+
+
+   
+
+}
---
+++ String_in_C++/PW_Learning/Lecture_2/8_Sub_String.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main(){
+    string name = "Sourabha!";
+    cout << name.substr(0,4) <<endl;
+    cout << name.substr(1,4) <<endl;
+   cout << name.substr(2) <<endl;
+}
