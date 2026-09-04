74c057d9bd590886ccb1c1e22b303d8a07fd415f
Sourabha Jena <sourabhajena2003@gmail.com>
2026-09-04 09:34:34 +0530

    6_Optimal_solution_Move_All_Zero_to_end.cpp

---
+++ Sorting_DSA/HomeWork/6_Optimal_solution_Move_All_Zero_to_end.cpp
@@ -0,0 +1,30 @@
+#include<iostream>
+#include<algorithm>
+#include <vector>
+using namespace std;
+void print(vector<int>& arr){
+     for(int a:arr){
+        cout <<a <<" ";
+    }
+  cout <<endl;
+}
+
+int main(){
+     vector<int> arr = {2,0,0,2,0,1,6,8,5,0,0};
+     cout <<"array :";
+     print(arr);  
+     
+     cout <<"Optimal Soution: ";
+     int n = arr.size();
+     int j = 0;
+
+     for(int i = 0; i < n - 1; i++){
+        if(arr[i] != 0){
+          swap(arr[i],arr[j]);
+          j++;
+        }
+     }
+     print(arr);
+
+     
+}
