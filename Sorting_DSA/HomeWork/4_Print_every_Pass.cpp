08813aef458d77d70dbdf24833334733cdf2072b
Sourabha Jena <sourabhajena2003@gmail.com>
2026-09-04 08:37:57 +0530

    4_Print_every_Pass.cpp

---
+++ Sorting_DSA/HomeWork/4_Print_every_Pass.cpp
@@ -0,0 +1,33 @@
+#include<iostream>
+#include<vector>
+#include<algorithm>
+using namespace std;
+
+void print(vector<int>& arr){
+     for(int a:arr){
+        cout <<a <<" ";
+    }
+  cout <<endl;
+}
+
+int main(){
+      vector<int> arr = {12,32,11,10,6,5,13,30,1};
+      cout <<"Unsorted array: ";
+      print(arr);
+      
+      int n = arr.size();
+      for(int i = 0; i < n -1; i++){
+        bool swapped = false;
+        for(int j = 0; j< n - 1 - i;j++){
+           if(arr[j] > arr[j + 1]){
+                swap(arr[j],arr[j+1]);
+                swapped = true;
+           } 
+        }
+        if(!swapped) break;
+        cout <<"after swap: " <<i + 1 <<": ";
+        print(arr);
+      }
+      cout << "Sorted array: ";
+      print(arr);
+}
