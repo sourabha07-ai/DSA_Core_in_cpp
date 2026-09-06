9b3e37c7ba6aa406fa7c1d800f4f692294225f82
Sourabha Jena <sourabhajena2003@gmail.com>
2026-09-06 08:16:36 +0530

    Remove element from sorted array

---
+++ Sorting_DSA/HomeWork/Bubble_Sort/17_Remove_dublicate_Sorted_array.cpp
@@ -0,0 +1,33 @@
+ #include <iostream>
+#include <vector>
+using namespace std;
+
+void print(const vector<int>& arr, int size) {
+    for (int i = 0; i < size; i++) {
+        cout << arr[i] << " ";
+    }
+    cout << endl;
+}
+
+int main() {
+
+    vector<int> arr = {1, 1, 2, 2, 3, 4, 4, 5};
+    int n = arr.size();
+    cout << "Array: ";
+    print(arr, n);
+
+    int j = 0;
+
+    // Remove duplicates in-place
+    for (int i = 1; i < n; i++) {
+        if (arr[i] != arr[i - 1]) {
+            j++;
+            arr[j] = arr[i];
+        }
+    }
+
+    cout << "After removing duplicates: ";
+    print(arr, j + 1);
+
+    return 0;
+}
