444f025636992d42eb0bfd91b45aa18e7d2446cf
Sourabha Jena <sourabhajena2003@gmail.com>
2026-09-06 07:39:41 +0530

    8_move_all_zero_to_the_End.cpp

--- Sorting_DSA/HomeWork/Bubble_Sort/8_move_all_zero_to_the_End.cpp
+++ Sorting_DSA/HomeWork/Bubble_Sort/8_move_all_zero_to_the_End.cpp
@@ -15,17 +15,20 @@ int main(){
 
         cout <<"All zero to the end: ";
         int n = arr.size();
+        int count = 0;
         for(int i = 0; i < n - 1; i++){
            bool swapped = false;
              for(int j = 0; j < n - 1 - i;j++){
                  if(arr[j] == 0 and arr[j + 1] != 0 ){
                         swap(arr[j], arr[j + 1]);
                         swapped = true;
+                        count++;
                  }
              }
              if(!swapped) break;
         }
         print(arr);
+        cout<<"swaps "<<count <<endl;
   
     return 0;
 }
