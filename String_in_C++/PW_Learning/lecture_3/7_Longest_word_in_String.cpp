b7d00c30ae5b29341e1e10a6dec1a01bff8c16a2
Sourabha Jena <sourabhajena2003@gmail.com>
2026-09-07 19:43:03 +0530

    add something

---
+++ String_in_C++/PW_Learning/lecture_3/7_Longest_word_in_String.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <string>
+using namespace std; 
+
+int main(){
+        string str = "Arjun is the Smartest boy";
+        int n = str.length(); 
+
+        cout <<n <<endl;
+  
+    return 0;
+}
