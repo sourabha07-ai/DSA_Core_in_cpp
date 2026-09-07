fdfbaefad1fcc59af1d614ab779db6d8cf3310dc
Sourabha Jena <sourabhajena2003@gmail.com>
2026-09-07 21:00:05 +0530

    1_FunctionCalling.cpp

---
+++ Recursion/1_FunctionCalling.cpp
@@ -0,0 +1,23 @@
+#include<iostream>
+using namespace std;
+void aryan(){
+        cout <<"Aryan" <<endl;
+
+}
+void param(){
+        aryan();
+        cout<<"Param"<<endl;
+        aryan();
+}
+void kartik(){
+        aryan();
+        cout <<"Kartik" <<endl;
+        param();
+}
+int main(){
+        kartik();
+        param();
+        aryan();
+
+    return 0;    
+}
