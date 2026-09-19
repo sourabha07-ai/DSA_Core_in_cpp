0c96fb5df1a9366b05e73719c421d45f9ec2bfe1
Sourabha Jena <sourabhajena2003@gmail.com>
2026-09-19 12:43:21 +0530

    1_Intertion_Sort.cpp

---
+++ 3_Sorting_DSA/Insertion_Sort/1_Intertion_Sort.cpp
@@ -0,0 +1,27 @@
+#include<iostream>
+#include<vector>
+using namespace std;
+void print(const vector<int>& arr){
+     for(int a:arr){
+        cout <<a <<" ";
+    }
+  cout <<endl;
+}
+
+int main(){
+        vector<int> arr = {4,2,6,1,8,5,3,7,9};
+        cout <<"Array: ";print(arr);
+
+        //Size of array
+        size_t n = arr.size();
+        for(int i = 0; i < n-1; i++){
+             int j = i;
+               while(j >= 1 && arr[j] < arr[j - 1]){
+                  swap(arr[j],arr[j - 1]);
+                  j--;
+               }   
+        }
+        cout <<"Insertion Sort array: ";print(arr);
+
+   
+}
