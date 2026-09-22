27c369963c611aee5194a16cb39c48e2696c1423
Sourabha Jena <sourabhajena2003@gmail.com>
2026-09-22 06:47:22 +0530

    5_User_Defined_DataStructure_Create_array.cpp

---
+++ 7_PW_Oops/Lecture_2/5_User_Defined_DataStructure_Create_array.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <vector>
+using namespace std; 
+
+
+int main(){
+    int* arr  = new int[6];
+    arr[0] = 20;
+
+    int b[] = {28,27};
+    arr = b;
+    cout<<arr[0]<<endl;
+  
+    return 0;
+}
